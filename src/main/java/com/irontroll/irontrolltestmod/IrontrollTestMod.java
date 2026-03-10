b0ff85bbe7bb6d6af70afba6ac3b77ce7ffc5aa1
unknown <irontroll4316@gmail.com>
2026-03-09 21:00:16 -0400

    Initialized Project

---
+++ src/main/java/com/irontroll/irontrolltestmod/IrontrollTestMod.java
@@ -0,0 +1,38 @@
+package com.irontroll.irontrolltestmod;
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+
+@Mod(IrontrollTestMod.MODID)
+public class IrontrollTestMod {
+    public static final String MODID = "irontrolltestmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public IrontrollTestMod(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
+        NeoForge.EVENT_BUS.register(this);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+       
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
